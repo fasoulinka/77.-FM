8da0763cb1ca62cc3ad27b981e58f6d8ce78eac9
Kamilla Serikbay <167647170+fasoulinka@users.noreply.github.com>
2025-09-28 22:55:44 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center p-8">
+      <h1 className="text-4xl font-bold mb-8">Vibe Match</h1>
+      <p>Image analyzer coming soon</p>
+    </div>
+  );
+}
